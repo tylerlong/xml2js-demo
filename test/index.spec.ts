f9f3d12ca575f1b69d3915a3730f7d30907974fb
Tyler Liu <tyler4long@gmail.com>
2024-08-22 16:55:40 -0700

    First working version

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,14 +1,48 @@
 import { expect, test } from 'vitest';
-import dotenv from 'dotenv-override-true';
+import xml2js from 'xml2js';
 
-import add from '../src/index';
+const xmlStr =
+  '<Msg><Hdr SID="36523388888888" Req="" Cmd="9" From="#1534016@sip.ringcentral.com:5060" To="17206668888*11115@sip.ringcentral.com:5060"/><Bdy Cln="802388888888" Phn="WIRELESS CALLER" Nm="+16506668888" ExtNfo="" Resp="0" Sts="0"/></Msg>';
 
-dotenv.config();
+const json = {
+  Msg: {
+    Hdr: {
+      $: {
+        SID: '36523388888888',
+        Req: '',
+        Cmd: '9',
+        From: '#1534016@sip.ringcentral.com:5060',
+        To: '17206668888*11115@sip.ringcentral.com:5060',
+      },
+    },
+    Bdy: {
+      $: {
+        Cln: '802388888888',
+        Phn: 'WIRELESS CALLER',
+        Nm: '+16506668888',
+        ExtNfo: '',
+        Resp: '0',
+        Sts: '0',
+      },
+    },
+  },
+};
 
-test('Addition', () => {
-  expect(add(1, 2)).toBe(3);
+test('parse XML', async () => {
+  const parser = new xml2js.Parser({
+    explicitArray: false,
+  });
+  const result = await parser.parseStringPromise(xmlStr);
+  expect(result).toMatchObject(json);
 });
 
-test('Load env vars', () => {
-  expect(process.env.NAME).toBe('Tyler Liu');
+test('build XML', async () => {
+  const builder = new xml2js.Builder({
+    renderOpts: {
+      pretty: false,
+    },
+    headless: true,
+  });
+  const xml = builder.buildObject(json);
+  expect(xml).toBe(xmlStr);
 });
